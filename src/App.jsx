74b29001b930e24ddc8db879077929884dc2bfa1
BigBoyz2314 <zazimran@gmail.com>
2024-01-02 02:18:28 +0500

    added services section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
 
 const App = () => {
   return <div>
@@ -10,7 +11,7 @@ const App = () => {
       <Hero/>
     </section>
     <section><Parallax type="services" /></section>
-    <section id="Services">Services</section>
+    <section id="Services"><Services/></section>
     <section><Parallax type="portfolio" /></section>
     <section id="Portfolio">Portfolio1</section>
     <section>Portfolio2</section>
--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,19 +1,33 @@
+import { useRef } from "react"
 import "./parallax.scss"
+import { motion, useScroll, useTransform } from "framer-motion"
 
 const Parallax = ({type}) => {
+
+  const ref = useRef()
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"]
+  })
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "300%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
+
   return (
     <div 
-    className="parallax" 
+    className="parallax"
+    ref={ref}
     style={{
-        background:type === "services"
-        ? "linear-gradient(180deg, #0c0c1d, #111132);" 
-        : "linear-gradient(180deg, #0c0c1d, #505064);"
+        background: type === "services"
+        ? "linear-gradient(180deg, #0c0c1d, #111132)"
+        : "linear-gradient(180deg, #0c0c1d, #505064)"
         }}
     >
-        <h1>{type === "services" ? "What We Do?" : "What We Did!"}</h1>
-        <div className="mountains"></div>
-        <div className="planets"></div>
-        <div className="stars"></div>
+        <motion.h1 style={{ y: yText }}>{type === "services" ? "What We Do?" : "What We Did!"}</motion.h1>
+        <motion.div className="mountains"></motion.div>
+        <motion.div style={{ y: yBg, backgroundImage: `url(${type === "services" ? "/planets.png" : "/sun.png"})`}} className="planets"></motion.div>
+        <motion.div style={{ x: yBg }}  className="stars"></motion.div>
     </div>
   )
 }

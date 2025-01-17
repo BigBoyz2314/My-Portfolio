e95a95b55f87d70cd78ae64231b80aa3ce77dec5
BigBoyz2314 <zazimran@gmail.com>
2025-01-17 19:06:15 +0500

    more updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,9 +14,9 @@ const Navbar = () => {
               Zain Imran
             </motion.span>
             <div className="social">
-                <a href="#"><img src="/linkedin.png" alt="" /></a>
-                <a href="#"><img src="/instagram.png" alt="" /></a>
-                <a href="#"><img src="/github.png" alt="" /></a>
+                <a href="https://www.linkedin.com/in/zain-imran2573/"><img src="/linkedin.png" alt="" /></a>
+                <a href="https://www.instagram.com/zain_bigboyzz/"><img src="/instagram.png" alt="" /></a>
+                <a href="https://github.com/BigBoyz2314"><img src="/github.png" alt="" /></a>
                 <a href="#"><img src="/dribbble.png" alt="" /></a>
             </div>
         </div>
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -23,53 +23,61 @@ const Services = () => {
 
     const ref = useRef()
 
-    const isInView = useInView(ref,{margin: "-100px"})
+    const isInView = useInView(ref, { margin: "-100px" })
 
-  return (
-    <motion.div className="services" variants={variants} initial="initial" ref={ref} animate={isInView && "animate"}>
-        <motion.div className="textContainer" variants={variants}>
-            <p>I focus on helping your brand grow 
-            <br /> and move forward</p>
-            <hr />
+    const handleClick = (e, item) => {
+        e.preventDefault();
+        const element = document.getElementById(item);
+        if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+
+    return (
+        <motion.div className="services" variants={variants} initial="initial" ref={ref} animate={isInView && "animate"}>
+            <motion.div className="textContainer" variants={variants}>
+                <p>I focus on helping your brand grow
+                    <br /> and move forward</p>
+                <hr />
+            </motion.div>
+            <motion.div className="titleContainer" variants={variants}>
+                <div className="title">
+                    <img src="/people.webp" alt="" />
+                    <h1>
+                        <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
+                    </h1>
+                </div>
+                <div className="title">
+                    <h1>
+                        <motion.b whileHover={{ color: "orange" }}>For Your</motion.b> Business.
+                    </h1>
+                    <button>What We Do?</button>
+                </div>
+            </motion.div>
+            <motion.div className="listContainer" variants={variants}>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>Custom Web Development</h2>
+                    <p>Crafting bespoke websites tailored to your unique needs, combining cutting-edge technology, responsive design, and seamless user experience to bring your vision to life.</p>
+                    <button onClick={(e) => handleClick(e, "Contact")}>Go</button>
+                </motion.div>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>Wix Development</h2>
+                    <p>Building dynamic and user-friendly websites on Wix, leveraging custom functionality, seamless integrations, and stunning designs to elevate your online presence.</p>
+                    <button onClick={(e) => handleClick(e, "Contact")}>Go</button>
+                </motion.div>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>PHP Development</h2>
+                    <p>Creating robust and efficient backend solutions with PHP, specializing in dynamic websites, custom applications, and seamless database integration for optimal performance.</p>
+                    <button onClick={(e) => handleClick(e, "Contact")}>Go</button>
+                </motion.div>
+                <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
+                    <h2>Next.js / React.js Development</h2>
+                    <p>Developing modern, high-performance web applications with Next.js and React, focusing on scalability, seamless user interfaces, and optimized server-side rendering for a superior user experience.</p>
+                    <button onClick={(e) => handleClick(e, "Contact")}>Go</button>
+                </motion.div>
+            </motion.div>
         </motion.div>
-        <motion.div className="titleContainer" variants={variants}>
-            <div className="title">
-                <img src="/people.webp" alt="" />
-                <h1>
-                    <motion.b whileHover={{color: "orange"}}>Unique</motion.b> Ideas
-                </h1>
-            </div>
-            <div className="title">
-                <h1>
-                    <motion.b whileHover={{color: "orange"}}>For Your</motion.b> Business.
-                </h1>
-                <button>What We Do?</button>
-            </div>
-        </motion.div>
-        <motion.div className="listContainer" variants={variants}>
-            <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
-                <h2>Branding</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique quibusdam nemo delectus, quas necessitatibus consequuntur quia suscipit aperiam quod!</p>
-                <button>Go</button>
-            </motion.div>
-            <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
-                <h2>Branding</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique quibusdam nemo delectus, quas necessitatibus consequuntur quia suscipit aperiam quod!</p>
-                <button>Go</button>
-            </motion.div>
-            <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
-                <h2>Branding</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique quibusdam nemo delectus, quas necessitatibus consequuntur quia suscipit aperiam quod!</p>
-                <button>Go</button>
-            </motion.div>
-            <motion.div className="box" whileHover={{background: "lightgray", color: "black"}}>
-                <h2>Branding</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique quibusdam nemo delectus, quas necessitatibus consequuntur quia suscipit aperiam quod!</p>
-                <button>Go</button>
-            </motion.div>
-        </motion.div>
-    </motion.div>
-  )
+    )
 }
 
 export default Services
